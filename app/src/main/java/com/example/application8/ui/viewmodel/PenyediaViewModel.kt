3b5b33a46cfdbc097ec5cf7dd2012da67f9ff29f
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-29 14:47:59 +0700

    Membuat PenyediaViewModel yang mengelola dan menginitializer seluruh viewmodel

---
+++ app/src/main/java/com/example/application8/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.application8.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.application8.MahasiswaApplications
+
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiMhs().container.mahasiswaRepository
+            )
+        }
+        initializer {
+            InsertViewModel(
+                aplikasiMhs().container.mahasiswaRepository
+            )
+        }
+    }
+}
+
+fun CreationExtras.aplikasiMhs(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
