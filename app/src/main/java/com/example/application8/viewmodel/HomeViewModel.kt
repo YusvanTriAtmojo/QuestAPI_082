597287a8aed84d3961c8db2a0140b3c354ac1768
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-29 10:58:59 +0700

    Membuat class untuk ViewModel Home untuk mengelola UI State dengan fungsi getMhs

--- app/src/main/java/com/example/application8/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/application8/viewmodel/HomeViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.application8.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.application8.model.Mahasiswa
+import com.example.application8.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
@@ -8,5 +17,24 @@ sealed class HomeUiState {
     object Loading : HomeUiState()
 }
 
-class HomeViewModel {
+class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel() {
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
 }
