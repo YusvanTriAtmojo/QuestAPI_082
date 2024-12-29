67a2b1276b493029db25960aa7a4192ec599a737
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-29 09:19:31 +0700

    Menghubungkan fungsi operasi di service dengan class NetworkMahasiswaRepository

--- app/src/main/java/com/example/application8/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/application8/repository/MahasiswaRepository.kt
@@ -1,6 +1,8 @@
 package com.example.application8.repository
 
 import com.example.application8.model.Mahasiswa
+import com.example.application8.service.MahasiswaService
+import java.io.IOException
 
 interface MahasiswaRepository {
     suspend fun getMahasiswa(): List<Mahasiswa>
@@ -9,3 +11,36 @@ interface MahasiswaRepository {
     suspend fun deleteMahasiswa(nim: String)
     suspend fun getMahasiswabyNim(nim: String): Mahasiswa
 }
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try{
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete mahasiswa. HTTP Status code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiwa()
+    override suspend fun getMahasiswabyNim(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswabyNim(nim)
+    }
+}
+
