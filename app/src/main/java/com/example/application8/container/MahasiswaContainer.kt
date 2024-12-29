e1d604714427f00f6193b751633dcac492e07ee6
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-29 09:30:08 +0700

    Membuat interface AppContainer yang terhubung dengan MahasiswaRepository

---
+++ app/src/main/java/com/example/application8/container/MahasiswaContainer.kt
@@ -0,0 +1,8 @@
+package com.example.application8.container
+
+import com.example.application8.repository.MahasiswaRepository
+
+interface AppContainer{
+    val kontakRepository: MahasiswaRepository
+}
+
