ba11344422883707512bba61063e1154aca05e7f
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-29 10:12:33 +0700

    Membuat Event insert UI berupa data class

---
+++ app/src/main/java/com/example/application8/viewmodel/InsertViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.application8.viewmodel
+
+class InsertViewModel {
+}
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jensiKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
