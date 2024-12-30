3ed82d3f3f18522bc32025ad7124624b70eb4ae6
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-30 09:53:56 +0700

    Membuat DetailUiState yang berisi variabel yang akan digunakan

--- app/src/main/java/com/example/application8/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/application8/ui/viewmodel/DetailViewModel.kt
@@ -2,6 +2,18 @@ package com.example.application8.ui.viewmodel
 
 import com.example.application8.model.Mahasiswa
 
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == InsertUiEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
 
 fun Mahasiswa.toDetailUiEvent(): InsertUiEvent {
     return InsertUiEvent(
