8fa4fb8a05bfef9e4043f0a462c7480f382d71c2
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-30 12:52:29 +0700

    Menambahkan composable Destinasi detail dan update ke dalam PengelolaHalaman

--- app/src/main/java/com/example/application8/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/application8/ui/navigation/PengelolaHalaman.kt
@@ -1,15 +1,20 @@
 package com.example.application8.ui.navigation
 
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.application8.ui.view.DestinasiDetail
 import com.example.application8.ui.view.DestinasiEntry
 import com.example.application8.ui.view.DestinasiHome
+import com.example.application8.ui.view.DestinasiUpdate
+import com.example.application8.ui.view.DetailView
 import com.example.application8.ui.view.EntryMhsScreen
 import com.example.application8.ui.view.HomeScreen
+import com.example.application8.ui.view.UpdateView
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
@@ -21,8 +26,12 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         composable(DestinasiHome.route){
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route)},
-                onDetailClick = {
-                }
+                onDetailClick = {nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                    println(
+                        "PengelolaHalaman: nim = $nim"
+                    )
+                },
             )
         }
         composable(DestinasiEntry.route){
@@ -33,6 +42,18 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                     }
                 }
             })
+        }
+        composable("${DestinasiDetail.route}/{nim}") { Detail ->
+            val nim = Detail.arguments?.getString("nim") ?: ""
+            DetailView(nim = nim, navigateBack = { navController.navigateUp() },
+                onEditClick = { nim ->
+                    navController.navigate("${DestinasiUpdate.route}/$nim")
+                })
+        }
+
+        composable("${DestinasiUpdate.route}/{nim}") { Update ->
+            val nim = Update.arguments?.getString("nim") ?: ""
+            UpdateView(navigateBack = { navController.navigateUp() })
         }
     }
 }
