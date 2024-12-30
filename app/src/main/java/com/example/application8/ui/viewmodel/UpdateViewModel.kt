d8765daad249df30d9f7261c9573da11752cc5e5
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-30 12:11:46 +0700

    Membuat UpdateViewModel yang mengelola pengupdatetan data mahasiswa

--- app/src/main/java/com/example/application8/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/application8/ui/viewmodel/UpdateViewModel.kt
@@ -1,6 +1,53 @@
 package com.example.application8.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.application8.model.Mahasiswa
+import com.example.application8.repository.MahasiswaRepository
+import com.example.application8.ui.view.DestinasiUpdate
+import kotlinx.coroutines.launch
+
+class UpdateViewModel(
+    private val repositoryMhs: MahasiswaRepository,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    var updateMhsUIState by mutableStateOf(InsertUiState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateMhsUIState = repositoryMhs.getMahasiswabyNim(_nim)
+                .toUIStateMhs()
+        }
+    }
+
+    fun updateState(insertUiEvent: InsertUiEvent) {
+        updateMhsUIState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    fun updateData() {
+        viewModelScope.launch {
+            try {
+                repositoryMhs.updateMahasiswa(_nim, updateMhsUIState.insertUiEvent.toMhs())
+                updateMhsUIState = updateMhsUIState.copy(
+                    snackBarMessage = "Data berhasil diupdate"
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+                updateMhsUIState = updateMhsUIState.copy(
+                    snackBarMessage = "Data gagal diupdate"
+                )
+            }
+        }
+    }
+}
 
 fun Mahasiswa.toUIStateMhs() : InsertUiState = InsertUiState(
     insertUiEvent = this.toDetailUiEvent(),
