f0ba805d80ae9b36ff87c85a11264ec63c91b646
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-30 10:24:20 +0700

    Membuat DetailViewModel yang mengelola pengambilan detail data

--- app/src/main/java/com/example/application8/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/application8/ui/viewmodel/DetailViewModel.kt
@@ -1,6 +1,37 @@
 package com.example.application8.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.application8.model.Mahasiswa
+import com.example.application8.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class DetailViewModel(private val mhs: MahasiswaRepository): ViewModel() {
+    var uiDetailState: DetailUiState by mutableStateOf(DetailUiState())
+        private set
+
+    fun getMhsDetail(nim: String) {
+        viewModelScope.launch {
+            uiDetailState = uiDetailState.copy(isLoading = true, isError = false)
+            try {
+                val DetailMhs = mhs.getMahasiswabyNim(nim)
+                uiDetailState = uiDetailState.copy(
+                    detailUiEvent = DetailMhs.toDetailUiEvent(),
+                    isLoading = false
+                )
+            } catch (e: Exception) {
+                uiDetailState = uiDetailState.copy(
+                    isError = true,
+                    errorMessage = "Error",
+                    isLoading = false
+                )
+            }
+        }
+    }
+}
 
 data class DetailUiState(
     val detailUiEvent: InsertUiEvent = InsertUiEvent(),
